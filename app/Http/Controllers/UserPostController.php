07cfed96164ab86c0a9a8a1014b63fc6b342027f
Ajith <ajithd78564@gmail.com>
2021-10-28 20:39:59 +0530

    controller

---
+++ app/Http/Controllers/UserPostController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserPostController extends Controller
+{
+    public function userData(Request $request)
+    {
+        
+    }
+}
